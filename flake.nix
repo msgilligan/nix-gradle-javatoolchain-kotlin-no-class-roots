8fadf9cef948adf9163601d2c8729205c1a3a3ac
Sean Gilligan <sean@msgilligan.com>
2024-12-02 01:59:58 +0000

    first commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Demo of Kotlin compile failure with pkgs.gradle with toolchain override";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ { flake-parts, devshell , ... }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { config, self', inputs', pkgs, system, lib, ... }: let
+        inherit (pkgs) stdenv;
+      in {
+        # define default devshell
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs ; [
+                (gradle.override {
+                    javaToolchains = [ jdk23 ]; # Put JDK 23 in Gradle's javaToolchain configuration
+                })
+            ];
+        };
+      };
+    };
+}
